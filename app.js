27d9c40d0f9f156fdc4664a467987adae1a440e9
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-11 11:04:10 +0300

    Практика на получение ссылок на HTML-элементы

---
+++ app.js
@@ -0,0 +1,11 @@
+const title = document.querySelector("h1");
+console.log(title);
+
+const btn = document.querySelector("#btn");
+console.log(btn);
+
+const subtitle = document.querySelector(".gray.title");
+console.log(subtitle);
+
+const sections = document.querySelectorAll("section");
+console.log(sections);
