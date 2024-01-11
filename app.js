ba73c2eed994a0d7387449d7042bbbde08f10b07
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-11 15:19:21 +0300

    InnerText, InnerHTML, getAttribute, setAttribute, href, classname

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ console.log(subtitle);
 
 const sections = document.querySelectorAll("section");
 console.log(sections);
+
+const anchor = document.querySelector(".a_api");
+console.log(anchor);
+console.log(anchor.innerText);
+anchor.innerText = "REST API";
+
+// const p = document.querySelector(".p_api");
+// console.log(p.innerText); // только текст
+// console.log(p.innerHTML); // вместе с html
+// p.innerHTML = "Hello <b> World</b>";
+
+const a = document.querySelector(".a_dom");
+console.log(a.getAttribute("href")); // получение атрибута href
+a.setAttribute("target", "_blank"); // установка нового атрибута target
+console.log(a);
+console.log(a.href); // выдает полный адрес ссылки
+console.log(a.className); // возвращает название класса
