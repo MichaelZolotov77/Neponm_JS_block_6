a0e997b5f131517be983afc27f4a411065e8d4d5
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-12 18:46:15 +0300

    removeEventListener()

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ function fn2() {
 title.onmouseenter = fn1; // событие наведения мыши - hover, с приставкой on
 
 title.addEventListener("click", fn2); // при клике отрабатывает событие
+
+title.onmouseenter = null; // снятие обработчика события
+
+title.removeEventListener("click", fn2); // cнятие обработчика
+// Лучше будет для EventListener задать функцию извне, так как при
+// removeEventListener функция, написанная внутри, продолжит работу
+
+title.addEventListener("click", function () {
+  console.log("Event!");
+});
+title.removeEventListener("click", function () {
+  console.log("Event!"); // это две разные функции
+});
