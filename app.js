d87f21c906de8dfab2b00f2f70aac9f2343c8df2
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-13 21:34:28 +0300

    Всплытие событий, stopPropagation()

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-const registeredUsers = [
-  ["admin", "KoI18"],
-  ["manager", "SuperMe108"],
-  ["editor", "12345"],
-];
+document.querySelector("button").addEventListener("click", handleClick);
+document.querySelector("section").addEventListener("click", handleClick);
+document.querySelector("div").addEventListener("click", handleClick);
 
-const inputs = document.querySelectorAll("input");
-const form = document.querySelector("form");
-const login = document.querySelector("[name=login]");
-const password = document.querySelector("[name=password]");
-
-inputs.forEach((input) => input.addEventListener("keypress", handleEvent));
-
-function handleEvent(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    event.target.nextElementSibling.focus();
-  }
-}
-
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-  event.preventDefault();
-  if (validate()) {
-    alert("Access granted");
-    console.log("Access granted");
-    form.reset();
-  }
-}
-
-function validate() {
-  if (!(login.value.trim() && password.value.trim())) {
-    alert("All fields are required");
-    console.log("All fields are required");
-    return false;
-  }
-  for (let user of registeredUsers) {
-    console.log(login.value, user[0]);
-    console.log(password.value, user[1]);
-    if (login.value === user[0] && password.value === user[1]) {
-      return true;
-    }
-  }
-  alert("Incorrect login or password");
-  console.log("Incorrect login or password");
-  return false;
+// если кликнуть кнопку, событие произойдет не только на кнопке
+// но и на div и на section
+function handleClick(event) {
+  event.stopPropagation(); // прекратить всплытие
+  console.log(event);
+  console.log(event.target);
 }
