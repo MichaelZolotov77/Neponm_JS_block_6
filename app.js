462f169bddc6904776285a095da55444c761081e
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-13 22:04:00 +0300

    Получение элемента, на котором произошло событие через this

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ document.querySelector("div").addEventListener("click", handleClick);
 // если кликнуть кнопку, событие произойдет не только на кнопке
 // но и на div и на section
 function handleClick(event) {
-  event.stopPropagation(); // прекратить всплытие
+  // event.stopPropagation(); // прекратить всплытие
   console.log(event);
   console.log(event.target);
+  console.log(this); // равно event.currentTarget
 }
