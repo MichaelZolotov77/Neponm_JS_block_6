cdf77affb8c91d3cf84f870b20f00a4f6e36c0f2
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-13 20:07:00 +0300

    Практика на работу с формой

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-const buttons = document.querySelectorAll("button");
+const registeredUsers = [
+  ["admin", "KoI18"],
+  ["manager", "SuperMe108"],
+  ["editor", "12345"],
+];
 
-function numberOfClicks(event) {
-  let clicks = event.target.dataset.clicked++;
-  console.log(`${event.target.id} click ${clicks + 1}`);
+const inputs = document.querySelectorAll("input");
+const form = document.querySelector("form");
+const login = document.querySelector("[name=login]");
+const password = document.querySelector("[name=password]");
+
+inputs.forEach((input) => input.addEventListener("keypress", handleEvent));
+
+function handleEvent(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    event.target.nextElementSibling.focus();
+  }
 }
 
-buttons.forEach((button) => button.addEventListener("click", numberOfClicks));
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (validate()) {
+    alert("Access granted");
+    console.log("Access granted");
+    form.reset();
+  }
+}
+
+function validate() {
+  if (!(login.value.trim() && password.value.trim())) {
+    alert("All fields are required");
+    console.log("All fields are required");
+    return false;
+  }
+  for (let user of registeredUsers) {
+    console.log(login.value, user[0]);
+    console.log(password.value, user[1]);
+    if (login.value === user[0] && password.value === user[1]) {
+      return true;
+    }
+  }
+  alert("Incorrect login or password");
+  console.log("Incorrect login or password");
+  return false;
+}
