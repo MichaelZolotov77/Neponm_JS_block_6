b9b25bdaac98a49a6b97c4c303be38e941949343
MichaelZolotov77 <michaelzol@yandex.ru>
2024-01-13 18:03:34 +0300

    Практика на работу с объектом события

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-const btn = document.querySelector("button");
-const inputs = document.querySelectorAll("input");
-const form = document.querySelector("form");
+const buttons = document.querySelectorAll("button");
 
-inputs.forEach((input) => input.addEventListener("keypress", handleEvent));
-form.addEventListener("submit", handleSubmit); // событие отправки
-
-function handleEvent(event) {
-  console.log(event);
-  console.dir(event.target); // элемент, на котором произошло событие
-
-  if (event.key === "Enter") {
-    event.preventDefault();
-    //если нажмем на enter, переместим фокус на следующего соседа
-    event.target.nextElementSibling.focus();
-  }
+function numberOfClicks(event) {
+  let clicks = event.target.dataset.clicked++;
+  console.log(`${event.target.id} click ${clicks + 1}`);
 }
 
-function handleSubmit(event) {
-  event.preventDefault(); // отменить действие по умолчанию
-  if (validate()) {
-    // submit AJAX - отправить форму, если валидная
-    form.reset(); // очистить все поля
-  } else {
-    alert("Fix empty fields");
-  }
-}
-
-function validate() {
-  let isValid = true;
-  inputs.forEach((input) => {
-    if (!input.value.trim()) {
-      input.style.borderColor = "red";
-      isValid = false;
-    } else {
-      input.style.borderColor = "currentColor";
-    }
-  });
-  return isValid;
-}
+buttons.forEach((button) => button.addEventListener("click", numberOfClicks));
